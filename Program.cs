753ee23eb1489ac988a72b13abc745c2ef917d4c
Angelica Gonzalez <71741248+marianAGtorres@users.noreply.github.com>
2020-09-25 16:52:20 -0500

    varibles de datos
    agregue las variables de los datos de nombre correo telefono y ocupacion con su repectiva funcion

--- Program.cs
+++ Program.cs
@@ -1,26 +1,46 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
 
-namespace Formulario_Basico
+namespace prueba_formulario
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int f;
-            string edad;
-            Console.WriteLine("Edad: ");
-            edad = Console.ReadLine();
-            f = int.Parse(edad);
-            if (f < 18)
+            string Nombre;
+            string Ocupacion;
+            string Correo;
+            string Telefono;
+            int a;
+
+            Console.WriteLine("Hola, Ingresa los siguienetes datos, para realizar tu registro");
+            Console.WriteLine("Nombre: ");
+            Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su edad:  ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ocupacion: ");
+            Ocupacion = Console.ReadLine();
+
+            Console.WriteLine("Correo: ");
+            Correo = Console.ReadLine();
+
+            Console.WriteLine("Telefono ");
+            Telefono = Console.ReadLine();
+
+
+            if (a >= 18)
             {
-                Console.WriteLine("Usted no se puede registrar por ser menor de edad");
+                Console.WriteLine("Bienvenido  tu registro ha sido Exitoso!");
 
             }
-
+            else
+            {
+                Console.WriteLine("Lo sentimos por ser menor de edad no puedes continuar");
+            }
 
         }
     }
-       
+}
+
 
