b0497f548aa0ae6079e03a62ec72bfb4b9f54739
Angelica Gonzalez <71741248+marianAGtorres@users.noreply.github.com>
2020-09-24 19:09:41 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Formulario_Basico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f;
+            string edad;
+            Console.WriteLine("Edad: ");
+            edad = Console.ReadLine();
+            f = int.Parse(edad);
+            if (f < 18)
+            {
+                Console.WriteLine("Usted no se puede registrar por ser menor de edad");
+
+            }
+
+
+        }
+    }
+       
+
